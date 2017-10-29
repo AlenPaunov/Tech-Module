c6f05906d81c570eac531ba09180f455f8398794
AlenPaunov <alen.paunov@gmail.com>
2017-10-29 17:10:19 +0200

    String and Text Processing Exercises

---
+++ StringsAndTextProcessingExercises/Character_Multiplier/Character_Multiplier.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_Multiplier
+{
+    class Character_Multiplier
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int totalSum = CharsMultiplier(input);
+
+            Console.WriteLine(totalSum);
+        }
+
+        private static int CharsMultiplier(string[] input)
+        {
+            char[] first = input[0].ToCharArray();
+            char[] second = input[1].ToCharArray();
+
+            int minLen = Math.Min(first.Length, second.Length);
+
+            int totalSum = 0;
+            for (int i = 0; i < minLen; i++)
+            {
+                totalSum += first[i] * second[i];
+            }
+            totalSum += first.Length > second.Length ? first.Skip(minLen).Sum(x => (int)x) : second.Skip(minLen).Sum(x => (int)x);
+            return totalSum;
+        }
+    }
+}
---
+++ StringsAndTextProcessingExercises/Convert_From_Base10_To_BaseN/Convert_From_Base10_To_BaseN.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Convert_From_Base10_To_BaseN
+{
+    class Convert_From_Base10_To_BaseN
+    {
+        static void Main(string[] args)
+        {
+            BigInteger[] input = Console.ReadLine().Split(' ').Select(x => BigInteger.Parse(x)).ToArray();
+
+            string result = ConvertToBase(input[1], input[0]);
+
+            Console.WriteLine(result);
+
+        }
+
+        static string ConvertToBase(BigInteger numberBaseTen, BigInteger baseToConvert)
+        {
+            Stack<BigInteger> converted = new Stack<BigInteger>();
+
+            while (numberBaseTen != 0)
+            {
+                BigInteger num = numberBaseTen % baseToConvert;
+                converted.Push(num);
+                numberBaseTen /= baseToConvert;
+            }
+
+            StringBuilder builder = new StringBuilder();
+            while (converted.Count != 0)
+            {
+                builder.Append(converted.Pop());
+            }
+            
+            return (builder.ToString());
+        }
+    }
+}
