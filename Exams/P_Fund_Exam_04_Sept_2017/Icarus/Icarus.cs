d2a815ff29814bf7f0a9cf6557d68636c4ec32bd
AlenPaunov <alen.paunov@gmail.com>
2017-11-04 17:15:08 +0200

    more exams

---
+++ Exams/P_Fund_Exam_04_Sept_2017/Icarus/Icarus.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Icarus
+{
+    class Icarus
+    {
+        static void Main(string[] args)
+        {
+            int[] thePlane = Console.ReadLine()
+                  .Split().Select(int.Parse).ToArray();
+
+            int index = int.Parse(Console.ReadLine());
+
+            int damage = 1;
+
+            string command = Console.ReadLine();
+            while (command != "Supernova")
+            {
+                string[] myArr = command.Split();
+                string direction = myArr[0];
+                int steps = int.Parse(myArr[1]);
+
+                switch (direction)
+                {
+                    case "left":
+
+                        for (int i = 0; i < steps; i++)
+                        {
+                            index--;
+
+                            if (index < 0)
+                            {
+                                index = thePlane.Length - 1;
+                                damage++;
+                            }
+                            thePlane[index] -= damage;
+                        }
+
+                        break;
+
+                    case "right":
+
+                        for (int i = 0; i < steps; i++)
+                        {
+                            index++;
+
+                            if (index > thePlane.Length - 1)
+                            {
+                                index = 0;
+                                damage++;
+                            }
+
+                            thePlane[index] -= damage;
+                        }
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", thePlane));
+        }
+    }
+    public class Phoenix
+    {
+        public int Damage;
+        public int Position;
+    }
+    public class Position
+    {
+        public int Health;
+        public bool IsNew = true;
+    }
+    public static class IcarusRotator
+    {
+        public static void Commander(this Phoenix phoenix, ref Dictionary<int, Position> plane, string command)
+        {
+            string[] commandTokens = command.Split(' ');
+            string direction = commandTokens[0];
+            int steps = int.Parse(commandTokens[1]);
+
+            if (steps == 0)
+            {
+                return;
+            }
+
+            switch (direction)
+            {
+                case "right":
+                    phoenix.GoRight(ref plane, steps);
+                    break;
+
+                case "left":
+                    phoenix.GoLeft(ref plane, steps);
+                    break;
+            }
+
+        }
+
+        public static void GoRight(this Phoenix phoenix, ref Dictionary<int, Position> plane, int steps)
+        {
+
+            int pos = phoenix.Position + 1;
+            for (int i = 1; i <= steps; i++)
+            {
+                if (pos >= plane.Count)
+                {
+                    pos = 0;
+                    phoenix.Damage++;
+
+                }
+                if (plane[pos].Health > 0)
+                {
+                    plane[pos].Health -= phoenix.Damage;
+
+                }
+                phoenix.Position = pos;
+                pos++;
+            }
+
+        }
+
+        public static void GoLeft(this Phoenix phoenix, ref Dictionary<int, Position> plane, int steps)
+        {
+            int pos = phoenix.Position - 1;
+
+            for (int i = 1; i <= steps; i++)
+            {
+                if (pos < 0)
+                {
+                    pos = plane.Count - 1;
+                    phoenix.Damage++;
+
+                }
+                if (plane[pos].Health > 0)
+                {
+                    plane[pos].Health -= phoenix.Damage;
+
+                }
+
+                phoenix.Position = pos;
+                pos--;
+            }
+        }
+    }
+}
---
+++ Exams/P_Fundamentals_Exam_June_16/SoftUni_Coffee_Supplies/SoftUni_Coffee_Supplies.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Management.Instrumentation;
+
+namespace SoftUni_Coffee_Supplies
+{
+    class SoftUni_Coffee_Supplies
+    {
+
+        static void Main(string[] args)
+        {
+            string[] delimiters = Console.ReadLine().Split();
+
+
+            string delimiterOne = delimiters[0];
+            string delimiterTwo = delimiters[1];
+
+            string line = Console.ReadLine();
+
+            List<Drinker> coffeeDrinkers = new List<Drinker>();
+            List<Coffee> coffeeTypes = new List<Coffee>();
+
+            while (line != "end of info")
+            {
+                if (Regex.IsMatch(line, $"(\\w+)(?:{Regex.Escape(delimiterOne)})(\\w+)"))
+                {
+                    string[] personAndCoffee = Regex.Split(line, Regex.Escape(delimiterOne));
+
+                    Drinker drinker = new Drinker
+                    {
+                        Name = personAndCoffee[0],
+                        Type = new Coffee { Name = personAndCoffee[1] }
+                    };
+                    if (!coffeeTypes.Any(x => x.Name == drinker.Type.Name))
+                    {
+                        coffeeTypes.Add(new Coffee { Name = personAndCoffee[1] });
+                    }
+
+                    coffeeDrinkers.Add(drinker);
+                }
+                else
+                {
+                    string[] coffeeAndQuantity = Regex.Split(line, Regex.Escape(delimiterTwo));
+
+                    Coffee coffee = new Coffee
+                    {
+                        Name = coffeeAndQuantity[0],
+                        Quantity = int.Parse(coffeeAndQuantity[1])
+                    };
+
+                    if (coffeeTypes.Any(x => x.Name == coffee.Name))
+                    {
+                        var indx = coffeeTypes.FindIndex(x => x.Name == coffee.Name);
+
+                        coffeeTypes[indx].Quantity += coffee.Quantity;
+                    }
+                    else
+                    {
+                        coffeeTypes.Add(coffee);
+                    }
+                }
+
+                line = Console.ReadLine();
+            }
+
+            line = Console.ReadLine();
+
+            foreach (var c in coffeeTypes.Where(x => x.Quantity <= 0))
+            {
+                Console.WriteLine($"Out of {c.Name}");
+            }
+
+            while (line != "end of week")
+            {
+                string[] tokens = line.Split();
+                string person = tokens[0];
+                int drinks = int.Parse(tokens[1]);
+
+                int indxP = coffeeDrinkers.FindIndex(x => x.Name == person);
+
+                string coffeeName = coffeeDrinkers[indxP].Type.Name;
+
+                int indxC = coffeeTypes.FindIndex(x => x.Name == coffeeName);
+
+
+                coffeeTypes[indxC].Quantity -= drinks;
+                if (coffeeTypes[indxC].Quantity <= 0)
+                {
+                    Console.WriteLine($"Out of {coffeeName}");
+                }
+
+                line = Console.ReadLine();
+            }
+            List<string> coffeeNames = new List<string>();
+
+            Console.WriteLine("Coffee Left:");
+            foreach (var c in coffeeTypes.Where(x => x.Quantity > 0).OrderByDescending(x => x.Quantity))
+            {
+                Console.WriteLine($"{c.Name} {c.Quantity}");
+                coffeeNames.Add(c.Name);
+            }
+
+            var results = new List<Drinker>();
+
+            foreach (var cName in coffeeNames)
+            {
+                foreach (var drinker in coffeeDrinkers)
+                {
+                    if (drinker.Type.Name == cName)
+                    {
+                        results.Add(drinker);
+                    }
+                }
+            }
+
+            Console.WriteLine("For:");
+
+            foreach (var drinker in results.OrderBy(x => x.Type.Name).ThenByDescending(p => p.Name))
+            {
+                Console.WriteLine($"{drinker.Name} {drinker.Type.Name}");
+            }
+        }
+    }
+
+    class Drinker
+    {
+        public string Name { get; set; }
+        public Coffee Type { get; set; }
+    }
+
+    class Coffee
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; } = 0;
+    }
+}
